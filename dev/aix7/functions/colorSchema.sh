bc253b49fe83b00c28a2ef22daeb2200e0bf8ad9
Alan da Silva Alves <linuxfama@gmail.com>
2022-04-14 19:12:03 -0300

    feat: color schema created

---
+++ dev/aix7/functions/colorSchema.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#  ============================== Header ==============================  #
+#  
+#  Name...............: colorSchema
+#  Version............: 0.1
+#  Description........: Color schema of use on apps
+#  Date...............: 04/14/2022
+#  Create.............: Alan da Silva Alves
+#  
+#  ============================== Header ==============================  #
+
+function colorSchema () {
+    ATTENTION='\033[1;93m'
+    INFORMATION='\033[95m'
+    SUCCESS='\033[92m'
+    DANGER='\033[1;31m'
+    LINE=''
+    BOLD='\033[1m'
+    CLEAN='\033[0m'
+}
+
