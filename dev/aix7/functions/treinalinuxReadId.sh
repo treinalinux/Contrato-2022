1e6ae8938f7c463052ac7e86cfb0fa71d0c9a5c5
Alan da Silva Alves <linuxfama@gmail.com>
2022-04-15 12:30:40 -0300

    feat: treinalinux readid started

---
+++ dev/aix7/functions/treinalinuxReadId.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#  ============================== Header ==============================  #
+#  
+#  Name...............: treinalinuxReadId
+#  Version............: 0.1
+#  Description........: Find with an list on file and salve result
+#  Date...............: 04/15/2022
+#  Create.............: Alan da Silva Alves
+#  
+#  ============================== Header ==============================  #
+
+source treinalinuxSearchEngine.sh
+
+function treinalinuxReadId() {
+    treinalinuxSearchEngine
+    result=Files/resultado.txt
+    my_list=Files/my_list.txt
+    list_id=$(grep -f "$my_list" "$result" | cut -d":" -f 1 | xargs)
+    echo "$list_id"
+}
+
+treinalinuxReadId
