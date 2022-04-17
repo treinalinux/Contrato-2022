fa688b2d8958bb1c1ea51ae6f75cbf2cbc325166
Alan da Silva Alves <linuxfama@gmail.com>
2022-04-17 09:41:42 -0300

    feat: called color schema added

--- dev/aix7/functions/treinalinuxAppendFile.sh
+++ dev/aix7/functions/treinalinuxAppendFile.sh
@@ -5,9 +5,19 @@
 #  Version............: 0.1
 #  Description........: Append data any in files
 #  Date...............: 04/16/2022
+#  Update at..........: 04/17/2022
+#                        - called color schema added
 #  Create.............: Alan da Silva Alves
 #  
 #  ============================== Header ==============================  #
+
+# Load the function:
+source treinalinuxColorSchema.sh
+
+# Declare variables
+SUCCESS=$(treinalinuxColorSchema SUCCESS)
+INFORMATION=$(treinalinuxColorSchema INFORMATION)
+CLEAN=$(treinalinuxColorSchema CLEAN)
 
 function treinalinuxAppendFile() {
     path_of_file_main="$1"
@@ -19,8 +29,7 @@ function treinalinuxAppendFile() {
     do
         count=$((count+1))
         tee -a ${path_of_file_main} <<< "$linha"
-
     done < $path_of_file_with_inputs
 
-    echo "Total de novas entradas: $count"
+    echo "\n${INFORMATION}Total de novas entradas:${CLEAN}${SUCCESS}${count}\n"
 }
